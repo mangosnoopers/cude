fa4539cd3dcd0a4c1a41d6784f350cda22eef7af
mlelien <emily.c.lien@gmail.com>
2019-02-16 13:29:29 -0500

    redirects

--- Assets/Scripts/CoolMathFunctions.cs
+++ Assets/Scripts/CoolMathFunctions.cs
@@ -1,21 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime.InteropServices;
 
 public class CoolMathFunctions : MonoBehaviour {
+	public string[] domains = new string[] {
+		"https://www.coolmath-games.com",
+		"www.coolmath-games.com",
+		"edit.coolmath-games.com",
+		"www.stage.coolmath-games.com",
+		"edit-stage.coolmath-games.com",
+		"dev.coolmath-games.com",
+		"m.coolmath-games.com",
+		"https://www.coolmathgames.com",
+		"www.coolmathgames.com",
+		"edit.coolmathgames.com",
+		"www.stage.coolmathgames.com",
+		"edit-stage.coolmathgames.com",
+		"dev.coolmathgames.com",
+		"m.coolmathgames.com"   
+	}; 
+
+	[DllImport("__Internal")]
+	private static extern void RedirectTo(string url);
+	// Check right away if the domain is valid
 
 	// Use this for initialization
 	void Start () {
-		// Import the javascript function that redirects to another URL   
-		[DllImport("__Internal")]privatestaticexternvoidRedirectTo();
+		CheckDomains();
+	}
 
-		// Import the javascript function that redirects to another URL   
-		[DllImport("__Internal")]privatestaticexternvoidStartGameEvent();
-
-		// Import the javascript function that redirects to another URL 
-		[DllImport("__Internal")]privatestaticexternvoidStartLevelEvent(int level);
-
-		// Import the javascript function that redirects to another URL   
-		[DllImport("__Internal")]privatestaticexternvoidReplayEvent();
+	private void CheckDomains() {
+		if (!IsValidHost(domains)) {
+			RedirectTo("www.coolmathgames.com");       
+		}   
+	}
+	
+	private bool IsValidHost(string[] hosts) {
+		foreach (string host in hosts)
+			if (Application.absoluteURL.IndexOf(host) == 0)
+				return true;
+		return false;
 	}
 }
