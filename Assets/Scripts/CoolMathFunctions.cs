75ebdb278bca0ff5bf0eb8f2757b5749d1891073
mlelien <emily.c.lien@gmail.com>
2019-02-16 11:19:20 -0500

    js to c# methods

---
+++ Assets/Scripts/CoolMathFunctions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoolMathFunctions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Import the javascript function that redirects to another URL   
+		[DllImport("__Internal")]privatestaticexternvoidRedirectTo();
+
+		// Import the javascript function that redirects to another URL   
+		[DllImport("__Internal")]privatestaticexternvoidStartGameEvent();
+
+		// Import the javascript function that redirects to another URL 
+		[DllImport("__Internal")]privatestaticexternvoidStartLevelEvent(int level);
+
+		// Import the javascript function that redirects to another URL   
+		[DllImport("__Internal")]privatestaticexternvoidReplayEvent();
+	}
+}
