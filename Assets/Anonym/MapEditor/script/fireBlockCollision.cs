20328fb960fa3ec9140ee15b8ca389f2698c1009
mlelien <emily.c.lien@gmail.com>
2019-03-04 23:46:52 -0500

    walls up

--- Assets/Anonym/MapEditor/script/fireBlockCollision.cs
+++ Assets/Anonym/MapEditor/script/fireBlockCollision.cs
@@ -3,28 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class fireBlockCollision : MonoBehaviour
-{
+public class fireBlockCollision : MonoBehaviour {
 
   public bool fireCollision = false;
 
-  void Start()
-  {
-    Debug.Log("fireBlockCollision start");
-  }
+  void OnControllerColliderHit (ControllerColliderHit hit) {
 
-  void OnControllerColliderHit(ControllerColliderHit hit)
-  {
-
-    if (hit.collider.tag == "FireBlock")
-    {
+    if (hit.collider.tag == "FireBlock") {
       fireCollision = true;
       //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
   }
 
-  public bool hasCollided()
-  {
+  public bool hasCollided () {
     return fireCollision;
   }
 }
